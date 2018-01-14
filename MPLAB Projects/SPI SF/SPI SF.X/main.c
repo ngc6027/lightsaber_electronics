9356df3cb0225ae511d81a1b8c97eec96744ce09
Michael Foreman <mike.seyfertssextet@gmail.com>
2018-01-13 20:31:31 -0500

    Added SPI initialization and interrupt handler skeleton for PMOD SF

---
+++ MPLAB Projects/SPI SF/SPI SF.X/main.c
@@ -0,0 +1,68 @@
+/* 
+ * File:   main.c
+ * Author: Michael Foreman
+ *
+ * Created on January 13, 2018, 6:14 PM
+ */
+
+// DEVCFG3
+// USERID = No Setting
+#pragma config FSRSSEL = PRIORITY_7     // SRS Select (SRS Priority 7)
+#pragma config FMIIEN = OFF             // Ethernet RMII/MII Enable (RMII Enabled)
+#pragma config FETHIO = OFF             // Ethernet I/O Pin Select (Alternate Ethernet I/O)
+#pragma config FCANIO = OFF             // CAN I/O Pin Select (Alternate CAN I/O)
+#pragma config FUSBIDIO = ON            // USB USID Selection (Controlled by the USB Module)
+#pragma config FVBUSONIO = ON           // USB VBUS ON Selection (Controlled by USB Module)
+
+// DEVCFG2
+#pragma config FPLLIDIV = DIV_2         // PLL Input Divider (2x Divider)
+#pragma config FPLLMUL = MUL_20         // PLL Multiplier (20x Multiplier)
+#pragma config UPLLIDIV = DIV_12        // USB PLL Input Divider (12x Divider)
+#pragma config UPLLEN = OFF             // USB PLL Enable (Disabled and Bypassed)
+#pragma config FPLLODIV = DIV_1         // System PLL Output Clock Divider (PLL Divide by 1)
+
+// DEVCFG1
+#pragma config FNOSC = PRIPLL           // Oscillator Selection Bits (Primary Osc w/PLL (XT+,HS+,EC+PLL))
+#pragma config FSOSCEN = ON             // Secondary Oscillator Enable (Enabled)
+#pragma config IESO = ON                // Internal/External Switch Over (Enabled)
+#pragma config POSCMOD = EC             // Primary Oscillator Configuration (External clock mode)
+#pragma config OSCIOFNC = OFF           // CLKO Output Signal Active on the OSCO Pin (Disabled)
+#pragma config FPBDIV = DIV_4           // Peripheral Clock Divisor (Pb_Clk is Sys_Clk/4)
+#pragma config FCKSM = CSDCMD           // Clock Switching and Monitor Selection (Clock Switch Disable, FSCM Disabled)
+#pragma config WDTPS = PS1048576        // Watchdog Timer Postscaler (1:1048576)
+#pragma config FWDTEN = OFF             // Watchdog Timer Enable (WDT Disabled (SWDTEN Bit Controls))
+
+// DEVCFG0
+#pragma config DEBUG = OFF              // Background Debugger Enable (Debugger is disabled)
+#pragma config ICESEL = ICS_PGx1        // ICE/ICD Comm Channel Select (ICE EMUC1/EMUD1 pins shared with PGC1/PGD1)
+#pragma config PWP = OFF                // Program Flash Write Protect (Disable)
+#pragma config BWP = OFF                // Boot Flash Write Protect bit (Protection Disabled)
+#pragma config CP = OFF                 // Code Protect (Protection Disabled)
+
+// #pragma config statements should precede project file includes.
+// Use project enums instead of #define for ON and OFF.
+
+#include <xc.h>
+#include <p32xxxx.h>
+#include <plib.h>
+#include <peripheral/spi.h>
+
+#include "pmodsf.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    initialize_spi();
+    INTEnableSystemMultiVectoredInt();
+    
+    while(1){
+        ;
+    }
+    return (EXIT_SUCCESS);
+}
+
